496002de1d04891e7198b70041a2d0c3407a00ea
GitHub Action <action@github.com>
2026-01-18 03:56:06 +0000

    chore: bump version to 0.0.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.0.3] - 2026-01-18
+### Added
+- Auto version bump via workflow
+
 ## [0.0.2] - 2026-01-14
 ### Added
 - Auto version bump via workflow
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -36,7 +36,7 @@ type Product struct {
 	Type_id        int64  `json:"Type"`
 }
 
-const version = "0.0.2"
+const version = "0.0.3"
 
 func main() {
 	err := godotenv.Load()
